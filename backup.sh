7f3126c8e189a6b146d83d50df7374d646cce881
meskill <8974488+meskill@users.noreply.github.com>
2023-09-23 21:29:21 +0000

    refactor: rewrite script from fish to nushell

--- backup.sh
+++ backup.sh
@@ -1,31 +1,25 @@
-#! /usr/bin/env fish
+#! /usr/bin/env nu
 
-read -P "Input Github Personal Access Token: " gh_token
+let gh_token = (input -s "Input Github Personal Access Token: ")
 
-echo "Cloning all the repos from github to local folder"
+print "\nCloning all the repos from github to local folder"
 
-./ghorg clone meskill \
-  --config=./ghorg-config.yml \
-  --ghorgignore-path=(pwd)/ghorgignore \
-  --path=(pwd) \
-  --token=$gh_token
+./ghorg clone meskill --config ./ghorg-config.yml --ghorgignore-path ./ghorgignore --path $env.PWD --token $gh_token
 
-and echo "Pushing all the repos to the sourcehut"
+print "\nPushing all the repos to the sourcehut"
 
-and for repo in ./**/HEAD
-  set -l dir (dirname $repo)
+ls ./**/HEAD | each {|it|
+  let dir = ($it.name | path dirname)
 
-  pushd $dir
+  cd $dir
 
-  git remote get-url sourcehut 2> /dev/null
+  try {
+    ^git remote get-url sourcehut err> /dev/null
+  } catch {|err|
+    let parsed = (^git remote get-url origin | parse -r "git@[^:]+:(?<repository_name>[^.]+).git");
 
-  if test $status -ne 0
-    set -l github_url (git remote get-url origin)
-    string match -riq "git@[^:]+:(?<repository_name>[^.]+).git" -- $github_url
+    ^git remote add sourcehut $"git@git.sr.ht:~($parsed.repository_name.0)"
+  }
 
-    git remote add sourcehut "git@git.sr.ht:~$repository_name"
-  end
-
-  git push sourcehut --all
-  popd
-end
+  ^git push sourcehut --all
+} | ignore
